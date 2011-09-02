9cf991d804ccb1226768e0e7a3b77b94997b1b8b
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2011-09-02 20:27:56 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scred::Application.config.secret_token = 'd981a4be4276483514f46a4f7d4ad98ad20da54bf05b3f3f8c179e4abc40603306cbfaf35126503e61d057b39c4129e97726c7e2a4c77d2914bbdaf56612057c'
